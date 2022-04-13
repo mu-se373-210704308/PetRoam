21dd451a2d11aa93e8aba0db9121d78242cd3676
İrem Kutnay <iremkutnay@gmail.com>
2022-04-13 23:53:48 +0300

    .

--- app/src/main/java/tr/edu/maltepe/project/petroam/login_page.kt
+++ app/src/main/java/tr/edu/maltepe/project/petroam/login_page.kt
@@ -1,14 +1,20 @@
 package tr.edu.maltepe.project.petroam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class login_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
 
-
+        val button: Button = findViewById(R.id.registerbutton)
+        button.setOnClickListener {
+            val intent = Intent(this, register_page::class.java)
+            startActivity(intent)
+        }
 
 
 
