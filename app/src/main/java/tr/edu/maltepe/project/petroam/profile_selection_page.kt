c400ed17bedbd197dae6936b5571c52e40b9051b
İrem Kutnay <iremkutnay@gmail.com>
2022-05-08 13:01:06 +0300

    .

---
+++ app/src/main/java/tr/edu/maltepe/project/petroam/profile_selection_page.kt
@@ -0,0 +1,11 @@
+package tr.edu.maltepe.project.petroam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profile_selection_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_selection_page)
+    }
+}
